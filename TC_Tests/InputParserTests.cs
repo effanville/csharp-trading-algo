f9271bf065ae1dcae128af31252d77442c95f32a
Effanville <>
2020-03-27 21:54:07 +0000

    Create Test project.

---
+++ TC_Tests/InputParserTests.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System;
+using TradingConsole.InputParser;
+
+namespace TC_Tests
+{
+    public class InputParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BasicInputsWork()
+        {
+            var inputArgs = "Simulate --StockFilePath \"C:\\Users\\masdoc\\source\\repos\\StockTradingConsole\\bin\\NewTextDocument.xml\" --StartDate 1/1/2019 --EndDate 28/2/2020 --StartingCash 20000";
+            string[] args = inputArgs.Split(' ');
+            var tokens = UIOGenerator.ParseUserInput(args, TestHelper.ReportLogger);
+            Assert.AreEqual(ProgramType.Simulate, tokens.funtionType);
+            Assert.AreEqual("\"C:\\Users\\masdoc\\source\\repos\\StockTradingConsole\\bin\\NewTextDocument.xml\"", tokens.StockFilePath);
+            Assert.AreEqual(new DateTime(2019, 1, 1), tokens.StartDate);
+            Assert.AreEqual(new DateTime(2020, 2, 28), tokens.EndDate);
+            Assert.AreEqual(20000, tokens.StartingCash);
+        }
+    }
+}
---
+++ TC_Tests/TestHelper.cs
@@ -0,0 +1,13 @@
+﻿using FinancialStructures.ReportLogging;
+
+namespace TC_Tests
+{
+    public static class TestHelper
+    {
+        public static LogReporter ReportLogger = new LogReporter((critical, type, location, message) => NothingFunction(critical, type, location, message));
+
+        private static void NothingFunction(string one, string two, string three, string four)
+        {
+        }
+    }
+}
